2f8d302f1557aca7cf687ddf59f98962dca1c567
Dmitriy Vasilenko <101889433+akiko23@users.noreply.github.com>
2023-04-25 16:54:30 +0300

    some comments

--- script.sh
+++ script.sh
@@ -1,15 +1,15 @@
 API_KEY=wYLMrI9ITvgE34F3cCEBmlcSXxalHPd8
-# useless const -> PYTHON_SCRIPT="import sys, json; data = json.load(sys.stdin); query = data['query']; print(query['amount'], query['from'], 'это', data['result'], query['to'])"
 
+# This function makes request on Fixer API which consists of amount, origin curency and curency that you want to receive and then returns the current rate
 function convert() {
     url="https://api.apilayer.com/fixer/convert?to=$3&from=$2&amount=$1"
-
     echo `curl -s --request GET \
          --url $url \
          --header "apikey: $API_KEY"` | \
     python3 -c "import sys, json; data = json.load(sys.stdin); query = data['query']; print(query['amount'], query['from'], 'это', data['result'], query['to'])"
 }
 
+# how to use instruction
 if [[ $1 == '-h' ]]; then
   echo ""
   exit 0
@@ -24,13 +24,11 @@ elif [[ -f $1 ]]; then
    done < $1 
 else
     read -p "Выберите вариант использования(1 - самостоятельный ввод запроса с клавиатуры в формате 'КОЛИЧЕСТВО ВАЛЮТА_НА_ВХОД ВАЛЮТА_НА_ВЫХОД' (5000 RUB USD); 2 - указание файла со строками формата 'КОЛИЧЕСТВО ВАЛЮТА_НА_ВХОД ВАЛЮТА_НА_ВЫХОД'): " action
-
     if [[ $action -eq 1 ]]; then
         read -p "Введите запрос в формате 'КОЛИЧЕСТВО ВАЛЮТА_НА_ВХОД ВАЛЮТА_НА_ВЫХОД' (5000 RUB USD): " amount from to
         convert $amount $from $to
     else
         read -p "Введите путь до файла: " f_path
-
         if [[ ! -f $f_path ]]; then
             echo "Указанного пути не существует или это не файл"
             exit 0
