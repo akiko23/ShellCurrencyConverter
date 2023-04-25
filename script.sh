0e22c7c2b3636647be8f29d9257f5b7ba14fba5a
Dmitriy Vasilenko <101889433+akiko23@users.noreply.github.com>
2023-04-25 16:40:22 +0300

    make the code more laconic

--- script.sh
+++ script.sh
@@ -1,39 +1,35 @@
-API_KEY=oZ2ugqoP2vSvDiu25QZzoWLwuYEV9WEC
+API_KEY=wYLMrI9ITvgE34F3cCEBmlcSXxalHPd8
 # useless const -> PYTHON_SCRIPT="import sys, json; data = json.load(sys.stdin); query = data['query']; print(query['amount'], query['from'], 'это', data['result'], query['to'])"
 
-if [[ $1 ]] && [[ $2 ]] && [[ $3 ]]; then
+function convert() {
     url="https://api.apilayer.com/fixer/convert?to=$3&from=$2&amount=$1"
 
     echo `curl -s --request GET \
          --url $url \
          --header "apikey: $API_KEY"` | \
     python3 -c "import sys, json; data = json.load(sys.stdin); query = data['query']; print(query['amount'], query['from'], 'это', data['result'], query['to'])"
+}
+
+if [[ $1 == '-h' ]]; then
+  echo ""
+  exit 0
+fi
+
+if [[ $1 ]] && [[ $2 ]] && [[ $3 ]]; then
+    convert $1 $2 $3
 elif [[ -f $1 ]]; then
    while read amount from to
    do
-       url="https://api.apilayer.com/fixer/convert?to=$to&from=$from&amount=$amount"
-
-        echo `curl -s --request GET \
-             --url $url \
-             --header "apikey: $API_KEY"` | \
-        python3 -c "import sys, json; data = json.load(sys.stdin); query = data['query']; print(query['amount'], query['from'], 'это', data['result'], query['to'])"
+      convert $amount $from $to
    done < $1 
 else
-    echo "Выберите вариант использования(1 - самостоятельный ввод запроса с клавиатуры в формате 'КОЛИЧЕСТВО ВАЛЮТА_НА_ВХОД ВАЛЮТА_НА_ВЫХОД' (5000 RUB USD); 2 - указание файла со строками формата 'КОЛИЧЕСТВО ВАЛЮТА_НА_ВХОД ВАЛЮТА_НА_ВЫХОД')"
-    read action
+    read -p "Выберите вариант использования(1 - самостоятельный ввод запроса с клавиатуры в формате 'КОЛИЧЕСТВО ВАЛЮТА_НА_ВХОД ВАЛЮТА_НА_ВЫХОД' (5000 RUB USD); 2 - указание файла со строками формата 'КОЛИЧЕСТВО ВАЛЮТА_НА_ВХОД ВАЛЮТА_НА_ВЫХОД'): " action
 
     if [[ $action -eq 1 ]]; then
-        echo "Введите запрос в формате 'КОЛИЧЕСТВО ВАЛЮТА_НА_ВХОД ВАЛЮТА_НА_ВЫХОД' (5000 RUB USD)"
-        read amount from to
-
-        url="https://api.apilayer.com/fixer/convert?to=$to&from=$from&amount=$amount"
-        echo `curl -s --request GET \
-             --url $url \
-             --header "apikey: $API_KEY"` | \
-        python3 -c "import sys, json; data = json.load(sys.stdin); query = data['query']; print(query['amount'], query['from'], 'это', data['result'], query['to'])"
+        read -p "Введите запрос в формате 'КОЛИЧЕСТВО ВАЛЮТА_НА_ВХОД ВАЛЮТА_НА_ВЫХОД' (5000 RUB USD): " amount from to
+        convert $amount $from $to
     else
-        echo "Введите путь до файла"
-        read f_path
+        read -p "Введите путь до файла: " f_path
 
         if [[ ! -f $f_path ]]; then
             echo "Указанного пути не существует или это не файл"
@@ -42,11 +38,7 @@ else
 
         while read amount from to
         do
-            url="https://api.apilayer.com/fixer/convert?to=$to&from=$from&amount=$amount"
-            echo `curl -s --request GET \
-                 --url $url \
-                 --header "apikey: $API_KEY"` | \
-            python3 -c "import sys, json; data = json.load(sys.stdin); query = data['query']; print(query['amount'], query['from'], 'это', data['result'], query['to'])"
+          convert $amount $from $to
         done < $f_path
     fi
 fi
