23bb03935e929bbd9933a014b2357ea35acb3915
Dmitriy <101889433+akiko23@users.noreply.github.com>
2023-05-17 22:17:58 +0300

    Update tests.sh

--- .tests/tests.sh
+++ .tests/tests.sh
@@ -7,7 +7,7 @@ error=\\033[31m[ERROR]\\033[37m
 info=\\033[35m[INFO]\\033[37m
 test_data_path=currencies
 
-chmod +x ../script.sh
+chmod +x ./script.sh
 
 # create files
 mkdir $test_data_path
@@ -17,7 +17,7 @@ echo -e "34 EUR RUB\n5 AMD fkaskfk" > $test_data_path/incorrect.txt
 echo 
 echo -e "$info TEST SCRIPT WITH ARGS ENTERED FROM KEYBOARD"
 echo -e "\n$sc Enter correct keyboard args"
-../script.sh 1 USD RUB > log # correct args (exit code has to be 0)
+./script.sh 1 USD RUB > log # correct args (exit code has to be 0)
 if [[ $? -eq 0 ]]; then
   echo -e "$success Test 1 passed" 
 else
@@ -25,7 +25,7 @@ else
 fi
 
 echo -e "\n$sc Enter incorrect keyboard args"
-../script.sh asdk 3 akfskkf > log # wrong args (exit code has to be 1)
+./script.sh asdk 3 akfskkf > log # wrong args (exit code has to be 1)
 if [[ $? -eq 1 ]]; then
   echo -e "$success Test 2 passed"
 else
@@ -35,7 +35,7 @@ echo
 
 echo -e "\n$info TEST SCRIPT WITH OPTION -f AND FILE PATH"
 echo -e "\n$sc Path to file is missing"
-../script.sh -f > log # necessary arg (path to file) is missing
+./script.sh -f > log # necessary arg (path to file) is missing
 if [[ $? -eq 2 ]]; then
   echo -e "$success Test 3 passed"
 else
@@ -43,7 +43,7 @@ else
 fi
 
 echo -e "\n$sc File doesn\`t exist"
-../script.sh -f kfdakfjd > log # file doesn`t exist (exit code has to be 3)
+./script.sh -f kfdakfjd > log # file doesn`t exist (exit code has to be 3)
 if [[ $? -eq 3 ]]; then
   echo -e "$success Test 4 passed"
 else
@@ -51,7 +51,7 @@ else
 fi
 
 echo -e "\n$sc Enter file with correct lines format"
-../script.sh -f $test_data_path/correct.txt > log # correct file lines format (exit code has to be 0)
+./script.sh -f $test_data_path/correct.txt > log # correct file lines format (exit code has to be 0)
 if [[ $? -eq 0 ]]; then
   echo -e "$success Test 5 passed"
 else
@@ -59,7 +59,7 @@ else
 fi
 
 echo -e "\n$sc Enter file with incorrect lines format"
-../script.sh -f $test_data_path/incorrect.txt > log # incorrect file lines format (exit code has to be 1)
+./script.sh -f $test_data_path/incorrect.txt > log # incorrect file lines format (exit code has to be 1)
 if [[ $? -eq 1 ]]; then
   echo -e "$success Test 6 passed"
 else
